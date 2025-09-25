66e7619b263d62e81b5b52710818105b28579941
Chen <ming@ChendeMac-mini.local>
2025-09-25 00:42:05 -0400

    add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+EMBEDDING_URL="http://test.2brain.cn:9800/v1/emb"
+RERANK_URL="http://test.2brain.cn:2260/rerank"
+IMAGE_MODEL_URL='http://test.2brain.cn:23333/v1'
+
+# Elasticsearch 认证配置
+ES_USERNAME="elastic"
+ES_PASSWORD="your_elasticsearch_password"  
+
+OPENAI_API_KEY="your_openai_api_key"  # 请替换为你的真实API密钥
+OPENAI_MODEL="gpt-5-nano"  # 或者使用 "gpt-4" 等其他模型
